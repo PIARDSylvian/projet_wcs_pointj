cdbf81ddb80633e4a33422b73b885d305c0e54b2
PIARDSylvian <piard.sylvian@gmail.com>
2016-03-24 17:00:02 +0100

    fixbug

--- contacts.php
+++ contacts.php
@@ -32,6 +32,9 @@
 					<iframe id="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2630.185188402435!2d1.2388716502758972!3d48.75925971553528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e157058e7ef151%3A0xa2151d47c555d65d!2s12+Route+Nationale%2C+28380+Saint-R%C3%A9my-sur-Avre!5e0!3m2!1sfr!2sfr!4v1458737274294" width="95%" height="85%" allowfullscreen></iframe>
 				</div>
 			</div>
+			<?php
+				include("./_footer.php");
+			?>
 		</section>
 	</body>
 </html>
